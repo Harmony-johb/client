d4bd025e75cb022202c361aecd07e5307396d116
hugobeguier <hugo.beguier@gmail.com>
2023-10-04 19:54:19 +0200

    Created Pages folder for architecture purposes

--- src/view/MainFrame.cpp
+++ src/view/MainFrame.cpp
@@ -20,16 +20,6 @@ void MainFrame::Initialize()
 	gb_sizer->SetFlexibleDirection(wxBOTH);
 	gb_sizer->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_NONE);
 
-	side_bar_1 = new SideBar1(this, wxID_ANY, wxDefaultPosition, wxSize(75, -1));
-	side_bar_2 = new SideBar2(this, wxID_ANY, wxDefaultPosition, wxSize(200, -1));
-	main_area = new MainArea(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
-	ctrl_area = new ControlArea(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
-	
-	gb_sizer->Add( side_bar_1, wxGBPosition( 0, 0 ), wxGBSpan( 2, 1 ), wxEXPAND);
-	gb_sizer->Add( side_bar_2, wxGBPosition( 0, 1 ), wxGBSpan( 1, 1 ), wxEXPAND);
-	gb_sizer->Add( main_area, wxGBPosition( 0, 2 ), wxGBSpan( 2, 1 ), wxEXPAND);
-	gb_sizer->Add( ctrl_area, wxGBPosition( 1, 1 ), wxGBSpan( 1, 1 ), wxEXPAND);
-
 	gb_sizer->AddGrowableCol(0, 0);
 	gb_sizer->AddGrowableCol(1, 0);
 	gb_sizer->AddGrowableCol(2, 1);
