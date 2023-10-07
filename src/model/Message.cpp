da203418fc8cacd901d50ff5f3d3008c363f19d1
maskenjesper <53495529+maskenjesper@users.noreply.github.com>
2023-10-07 17:40:09 +0200

    Implemented Message model

--- src/model/Message.cpp
+++ src/model/Message.cpp
@@ -10,7 +10,35 @@ LocalMessage::LocalMessage(int id)
 {
 }
 
+std::string LocalMessage::GetContent()
+{
+    return _content;
+}
+
+User *LocalMessage::GetSender()
+{
+    return _sender;
+}
+
 RemoteMessage::RemoteMessage(int id)
     : Message(id)
 {
+}
+
+std::string RemoteMessage::GetContent()
+{
+    InitializeLocalMessage();
+    return _local_message->GetContent();
+}
+
+User *RemoteMessage::GetSender()
+{
+    InitializeLocalMessage();
+    return _local_message->GetSender();
+}
+
+void RemoteMessage::InitializeLocalMessage()
+{
+    if (_local_message == nullptr)
+        _local_message = new LocalMessage(_id);
 }
